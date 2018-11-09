ce951f39712e6f06ec0bba1cd8c36b63a64f99fc
Alex Wright <adwright@msu.edu>
2018-11-09 17:06:24 -0500

    Updating R code

--- AWE_Day2_Exercise/Team R Code.R
+++ AWE_Day2_Exercise/Team R Code.R
@@ -1,3 +1,34 @@
 # Names: Alex Wright, Erin Larson, and Wynne Moss
 # Date: 9 Nov 2018
 # Description: Team exercise to compare the veg structure and canopy height 
+
+
+#########
+## PART - Setting up the example
+#########
+
+##
+#### INSTALL PACKAGES
+##
+
+#Library
+library(neonUtilities)
+library(geoNEON)
+library(raster)
+library(rhdf5)
+
+##
+#### DOWNLOAD & STACK DATA (veg structure and canopy height)
+##
+
+# download observational data with zipsByProduct()
+#Only have to do this step once (the first time you work the data)
+zipsByProduct(dpID = "DP1.10098.001", 
+              site = 'WREF', #site = all for all sites
+              package = 'expanded', 
+              check.size = T, # turn to false when using a continious workflow
+              savepath = "C:/Users/Al/Files/PHD/25_AUG_2016/ProjectManagement/NEON")  
+# now stack downloaded data
+stackByTable("C:/Users/Al/Files/PHD/25_AUG_2016/ProjectManagement/NEON/filesToStack10098/", folder = T)
+
+
